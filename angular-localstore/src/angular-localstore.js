5f85ba995654d80dc4807672f1fffaf825779cb8
brian.mcbrayer <Brian.McBrayer@dudesolutions.com>
2014-12-15 23:06:30 -0500

    Changed  to a provider

--- angular-localstore/src/angular-localstore.js
+++ angular-localstore/src/angular-localstore.js
@@ -15,14 +15,16 @@
  */
 
 angular.module('heroicVentures.localStore', [])
-.factory('$localStorage',
+.provider('$localStorage',
         ['$window',
 function ($window) {
     'use strict';
+    var me = this;
 
-    var ls;
+    me.$get = [function() {
+      var ls;
 
-    var service = {
+      var service = {
         setItem: setItem,
         getItem: getItem,
         clear: clear,
@@ -30,104 +32,104 @@ function ($window) {
         key: key,
         length: length,
         canUse: browserHasNative
-    };
+      };
 
-    ////////////
-    // Init
-    ////////////
-    init();
+      ////////////
+      // Init
+      ////////////
+      init();
 
-    ////////////
-    // Functions
-    ////////////
+      ////////////
+      // Functions
+      ////////////
 
-    function init() {
+      function init() {
         if (browserHasNative()) {
-            ls = $window.localStorage;
+          ls = $window.localStorage;
         } else {
-            // Create fallback here
+          // Create fallback here
         }
-    }
+      }
 
-    function browserHasNative() {
+      function browserHasNative() {
         var hasNative,
-            FAKE_ITEM_KEY = 'hv.fakeKey.asdfghjlkasdfghjlasdfghjkasdfghjklasdfghjklweqryuiozxcvnm',
-            FAKE_ITEM_VALUE = 'hv.fakeValue';
+        FAKE_ITEM_KEY = 'hv.fakeKey.asdfghjlkasdfghjlasdfghjkasdfghjklasdfghjklweqryuiozxcvnm',
+        FAKE_ITEM_VALUE = 'hv.fakeValue';
 
         try {
 
-            // General browser test
-            hasNative = ('localStorage' in $window);
+          // General browser test
+          hasNative = ('localStorage' in $window);
 
-            // Can we touch the object? (Fails in Chrome when localStorage is blocked)
-            var ls = $window.localStorage;
-            hasNative = hasNative && (ls != null);
+          // Can we touch the object? (Fails in Chrome when localStorage is blocked)
+          var ls = $window.localStorage;
+          hasNative = hasNative && (ls != null);
 
-            // Can we store something?
-            ls.setItem(FAKE_ITEM_KEY, FAKE_ITEM_VALUE);
-            hasNative = ls.getItem(FAKE_ITEM_KEY) != null;
+          // Can we store something?
+          ls.setItem(FAKE_ITEM_KEY, FAKE_ITEM_VALUE);
+          hasNative = ls.getItem(FAKE_ITEM_KEY) != null;
 
-            // Clear the fake key
-            ls.removeItem(FAKE_ITEM_KEY);
+          // Clear the fake key
+          ls.removeItem(FAKE_ITEM_KEY);
         } catch (e) {
-            hasNative = false;
+          hasNative = false;
         }
 
         return hasNative;
-    }
+      }
 
-    function length() {
+      function length() {
         return ls.length;
-    }
+      }
 
-    function key(index) {
+      function key(index) {
         return ls.key(index);
-    }
+      }
 
-    function removeItem(key) {
+      function removeItem(key) {
         return ls.removeItem(key);
-    }
+      }
 
-    function clear() {
+      function clear() {
         ls.clear();
-    }
+      }
 
-    function setItem(key, value, expiresOn) {
+      function setItem(key, value, expiresOn) {
         var expiryDate = (expiresOn != null ? new Date(expiresOn) : null);
         var wrappedItem = new LSItemWrapper(value, expiryDate);
 
         if (wrappedItem.isValid()) {
-            ls.setItem(key, wrappedItem.toStorageObject());
+          ls.setItem(key, wrappedItem.toStorageObject());
 
-            return value;
+          return value;
         }
 
         return null;
-    }
+      }
 
-    function getItem(key) {
+      function getItem(key) {
         var rawItem = ls.getItem(key);
 
         if (rawItem) {
-            var parsedItem = JSON.parse(rawItem);
+          var parsedItem = JSON.parse(rawItem);
 
-            if (LSItemWrapper.isLSItemWrapper(parsedItem)) {
-                var parsedLSItem = new LSItemWrapper(parsedItem.value, parsedItem.expiresOn);
-                if (parsedLSItem.isValid()) {
-                    return parsedItem.value;
-                } else {
-                    service.removeItem(key);
-                }
+          if (LSItemWrapper.isLSItemWrapper(parsedItem)) {
+            var parsedLSItem = new LSItemWrapper(parsedItem.value, parsedItem.expiresOn);
+            if (parsedLSItem.isValid()) {
+              return parsedItem.value;
             } else {
-                return parsedItem;
+              service.removeItem(key);
             }
+          } else {
+            return parsedItem;
+          }
         }
 
         return null;
-    }
+      }
 
-    LSItemWrapper.isLSItemWrapper = isLSItemWrapper;
-    function LSItemWrapper(value, expiresOn) {
+      LSItemWrapper.isLSItemWrapper = isLSItemWrapper;
+      function LSItemWrapper(value, expiresOn) {
         var me = this;
 
         handleExpiresOn();
@@ -138,31 +140,32 @@ function ($window) {
         me.isValid = isValid;
 
         function toStorageObject() {
-            return JSON.stringify(me);
+          return JSON.stringify(me);
         }
 
         function handleExpiresOn() {
-            // Return a null object if it's already past expiration
-            if (expiresOn instanceof Date) {
-                me.expiresOn = expiresOn.toISOString();
-            }
+          // Return a null object if it's already past expiration
+          if (expiresOn instanceof Date) {
+            me.expiresOn = expiresOn.toISOString();
+          }
         }
 
         function isValid() {
-            var parsedExpiresOn = Date.parse(me.expiresOn);
+          var parsedExpiresOn = Date.parse(me.expiresOn);
 
-            if (parsedExpiresOn) {
-                return parsedExpiresOn >= new Date();
-            }
+          if (parsedExpiresOn) {
+            return parsedExpiresOn >= new Date();
+          }
 
-            return true;
+          return true;
         }
-    }
+      }
 
-    function isLSItemWrapper(obj) {
+      function isLSItemWrapper(obj) {
         return obj.$$lsi;
-    }
+      }
 
 
-    return service;
+      return service;
+    }];
 }]);
