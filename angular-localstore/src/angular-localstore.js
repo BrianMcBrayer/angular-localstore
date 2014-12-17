9a692cba6eedbb03274e80579a2c975e2f65f98d
unknown <brandmcbrayer@live.com>
2014-12-16 20:26:09 -0500

    Refactored localStorage to separate module

--- angular-localstore/src/angular-localstore.js
+++ angular-localstore/src/angular-localstore.js
@@ -15,15 +15,14 @@
  */
 
 angular.module('heroicVentures.localStore', [])
-.provider('$localStorage',
+.provider('$localStore',
         [
 function () {
     'use strict';
     var me = this;
 
     me.setStorageServicesByPriority = setStorageServicesByPriority;
-    me.findHighestUsefulStorageService = findHighestUsefulStorageService;
-    
+
     //////////
     // Members
     //////////
@@ -34,8 +33,11 @@ function () {
     function setStorageServicesByPriority () {
       storageServicePriority = [];
 
-      // Convert the passed arguments to an array
-      if (arguments.length > 0) {
+      // Is the first argument an array?
+      if (arguments.length > 0 && Array.isArray(arguments[0])) {
+        storageServicePriority = arguments[0];
+      } else if (arguments.length > 1) {
+        // Convert the passed arguments to an array
         for (var curArgIndex = 0, len = arguments.length; curArgIndex < len; i++) {
           storageServicePriority.push(arguments[curArgIndex]);
         }
@@ -44,165 +46,43 @@ function () {
       return storageServicePriority;
     }
 
-    function findHighestUsefulStorageService() {
-      useStorageService = null;
-
-      for (var curArgIndex = 0, len = storageServicePriority.length; curArgIndex < len; i++) {
-        var curService = storageServicePriority[curArgIndex];
-
-        if (curService && typeof(curService.canUse) === 'function' && curService.canUse()) {
-          useStorageService = curService;
-          break;
-        }
-      }
-
-      return useStorageService;
-    }
-
-    me.$get = ['$window', function($window) {
-      var ls;
+    me.$get = [function() {
 
       var service = {
-        setItem: setItem,
-        getItem: getItem,
-        clear: clear,
-        removeItem: removeItem,
-        key: key,
-        length: length,
-        canUse: browserHasNative
+        hasAvailableService: hasAvailableService,
+        availableService: availableService
       };
 
-      ////////////
-      // Init
-      ////////////
-      init();
-
-      ////////////
-      // Functions
-      ////////////
-
-      function init() {
-        if (browserHasNative()) {
-          ls = $window.localStorage;
-        } else {
-          // Create fallback here
-        }
-      }
-
-      function browserHasNative() {
-        var hasNative,
-        FAKE_ITEM_KEY = 'hv.fakeKey.asdfghjlkasdfghjlasdfghjkasdfghjklasdfghjklweqryuiozxcvnm',
-        FAKE_ITEM_VALUE = 'hv.fakeValue';
-
+      function hasAvailableService() {
         try {
+          return (availableService() !== null);
+        } catch (variable) {
+          return false;
+        }
+      }
 
-          // General browser test
-          hasNative = ('localStorage' in $window);
-
-          // Can we touch the object? (Fails in Chrome when localStorage is blocked)
-          var ls = $window.localStorage;
-          hasNative = hasNative && (ls != null);
-
-          // Can we store something?
-          ls.setItem(FAKE_ITEM_KEY, FAKE_ITEM_VALUE);
-          hasNative = ls.getItem(FAKE_ITEM_KEY) != null;
-
-          // Clear the fake key
-          ls.removeItem(FAKE_ITEM_KEY);
-        } catch (e) {
-          hasNative = false;
+      function availableService() {
+        if (useStorageService === null || findHighestUsefulStorageService()) {
+          throw new Error('No available storage service exists');
         }
 
-        return hasNative;
+        return useStorageService;
       }
 
-      function length() {
-        return ls.length;
-      }
+      function findHighestUsefulStorageService() {
+        useStorageService = null;
 
-      function key(index) {
-        return ls.key(index);
-      }
+        for (var curArgIndex = 0, len = storageServicePriority.length; curArgIndex < len; i++) {
+          var curService = storageServicePriority[curArgIndex];
 
-      function removeItem(key) {
-        return ls.removeItem(key);
-      }
-
-      function clear() {
-        ls.clear();
-      }
-
-      function setItem(key, value, expiresOn) {
-        var expiryDate = (expiresOn != null ? new Date(expiresOn) : null);
-        var wrappedItem = new LSItemWrapper(value, expiryDate);
-
-        if (wrappedItem.isValid()) {
-          ls.setItem(key, wrappedItem.toStorageObject());
-
-          return value;
-        }
-
-        return null;
-      }
-
-      function getItem(key) {
-        var rawItem = ls.getItem(key);
-
-        if (rawItem) {
-          var parsedItem = JSON.parse(rawItem);
-
-          if (LSItemWrapper.isLSItemWrapper(parsedItem)) {
-            var parsedLSItem = new LSItemWrapper(parsedItem.value, parsedItem.expiresOn);
-            if (parsedLSItem.isValid()) {
-              return parsedItem.value;
-            } else {
-              service.removeItem(key);
-            }
-          } else {
-            return parsedItem;
+          if (curService && typeof(curService.canUse) === 'function' && curService.canUse()) {
+            useStorageService = curService;
+            break;
           }
         }
 
-        return null;
+        return useStorageService;
       }
-
-      LSItemWrapper.isLSItemWrapper = isLSItemWrapper;
-      function LSItemWrapper(value, expiresOn) {
-        var me = this;
-
-        handleExpiresOn();
-
-        me.value = value;
-        me.$$lsi = true;
-        me.toStorageObject = toStorageObject;
-        me.isValid = isValid;
-
-        function toStorageObject() {
-          return JSON.stringify(me);
-        }
-
-        function handleExpiresOn() {
-          // Return a null object if it's already past expiration
-          if (expiresOn instanceof Date) {
-            me.expiresOn = expiresOn.toISOString();
-          }
-        }
-
-        function isValid() {
-          var parsedExpiresOn = Date.parse(me.expiresOn);
-
-          if (parsedExpiresOn) {
-            return parsedExpiresOn >= new Date();
-          }
-
-          return true;
-        }
-      }
-
-      function isLSItemWrapper(obj) {
-        return obj.$$lsi;
-      }
-
 
       return service;
     }];
